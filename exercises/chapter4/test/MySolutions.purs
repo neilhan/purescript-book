0479adf0b35222c053255d684130a8c3acb99e60
Neil Han <neil.han@gmail.com>
2021-08-25 22:26:51 -0700

    chapter 4 started

--- exercises/chapter4/test/MySolutions.purs
+++ exercises/chapter4/test/MySolutions.purs
@@ -2,4 +2,23 @@ module Test.MySolutions where
 
 import Prelude
 
+import Data.Array (filter, length)
+
 -- Note to reader: Add your solutions to this file
+
+isEven :: Int -> Boolean
+-- isEven n = n / 2 * 2 == n
+isEven n = 
+    if n < 0 then
+        isEven (-n)
+    else if n == 0 then
+        true
+    else if n == 1 then
+        false
+    else
+        isEven $ n - 2
+
+
+-- countEven
+countEven :: Array Int -> Int
+countEven = (filter isEven) >>> length
